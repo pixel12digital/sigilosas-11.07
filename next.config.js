18d8c30fff29d4d016751223fad66804318b4bf3
pixel12digital <dietrich.representacoes@gmail.com>
2025-06-15 14:53:09 -0300

    Remover builds do vercel.json e env do next.config.js para corrigir build na Vercel

--- next.config.js
+++ next.config.js
@@ -11,11 +11,6 @@ const nextConfig = {
     ],
     formats: ['image/webp', 'image/avif'],
   },
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  },
   async headers() {
     return [
       {
